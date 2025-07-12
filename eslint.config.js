13fad14a4a660a4ead3010252002eef16787f8b1
jo4086 <ghzktm5892@gmail.com>
2025-07-12 23:15:47 +0900

    [Chore] eslint + tsconfig 경로 alias 통합 및 에러 수정
    [2025-07-12]

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
+// INFO: --COMMON--
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
-import pluginReact from 'eslint-plugin-react';
 import pluginImport from 'eslint-plugin-import';
 
+// INFO: --REACT--
+import pluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
+  // COMMENT: global settings
+  js.configs.recommended,
+  tseslint.configs.recommended,
   {
     languageOptions: {
       parser: tseslint.parser,
@@ -20,89 +24,29 @@ export default tseslint.config(
         },
       },
     },
-    plugins: {
-      react: pluginReact,
-      import: pluginImport,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    plugins: {},
     settings: {
       react: {
         version: 'detect',
       },
+      typescript: {
+        alwaysTryTypes: true,
+      },
       'import/resolver': {
-        typescript: {
-          // alwaysTryTypes: true,
-          // project: ['apps/*/tsconfig.json', 'packages/*/tsconfig.json'],
+        node: {
+          paths: ['src'],
         },
       },
     },
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-
-  // pluginReact.configs.recommended,
-  // pluginImport.configs.recommended,
-  // pluginImport.configs.typescript,
-
-  {
-    ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '*.config.js', 'pnpm-lock.yaml'],
-  },
-  {
-    files: ['apps/back/**/*.ts'],
-    languageOptions: {
-      globals: globals.node,
-    },
-    rules: {},
-  },
-
-  {
-    files: ['apps/front-end/**/*.{ts,tsx}'],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    extends: [],
-    rules: {
-      ...pluginReact.configs.recommended.rules,
-      ...reactHooks.configs['recommended-latest'].rules,
-      ...reactRefresh.configs.vite.rules,
-      'react/react-in-jsx-scope': 'off',
-    },
-  },
-  {
-    files: ['apps/front/**/*.{ts,tsx}'],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    extends: [],
-    rules: {
-      ...pluginReact.configs.recommended.rules,
-      ...reactHooks.configs['recommended-latest'].rules,
-      ...reactRefresh.configs.vite.rules,
-      'react/react-in-jsx-scope': 'off',
-    },
-  },
-
-  {
-    files: ['packages/**/*.ts'],
-    rules: {},
-  },
-
-  {
+    /* COMMENT: ESLint Flat Config
+     * - `extends` = must be array of plain objects (not strings, not spreads)
+     * - `rules`   = single plain object (spread allowed)
+     */
+    extends: [pluginImport.flatConfigs.recommended, pluginImport.flatConfigs.typescript],
     rules: {
       ...pluginImport.configs.recommended.rules,
       ...pluginImport.configs.typescript.rules,
-      // 'no-console': 'warn',
+      'import/no-unresolved': 'off',
       'no-unused-vars': [
         'warn',
         {
@@ -128,5 +72,48 @@ export default tseslint.config(
         },
       ],
     },
+  },
+  {
+    // COMMENT: Ignore Files
+    ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '*.config.js', 'pnpm-lock.yaml'],
+  },
+  {
+    // COMMENT: Back-End
+    files: ['apps/back/**/*.ts'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {},
+  },
+  {
+    // COMMENT: Front-End
+    files: ['apps/front/**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    extends: [],
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/array-type': 'error',
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+  },
+
+  {
+    // COMMENT: Package
+    files: ['packages/**/**/*.ts'],
+    rules: {},
   },
 );
