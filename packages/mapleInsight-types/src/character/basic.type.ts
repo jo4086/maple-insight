2591018b82aa3e343e5eea0b3ea6243ac0b6e87d
jo4086 <ghzktm5892@gmail.com>
2025-07-17 04:57:49 +0900

    [Refactor] types => mapleInsight-types 패키지 이름 변경 및 공통 타입 추가
    [2025-07-17]

---
+++ packages/mapleInsight-types/src/character/basic.type.ts
@@ -0,0 +1,35 @@
+const BasicApiResponseMap = {
+  date: 'date',
+  world_name: 'world',
+  character_name: 'name',
+  character_gender: 'gender',
+  character_class: 'class',
+  character_class_level: 'class_level',
+  character_exp: 'exp',
+  character_exp_rate: 'exp_percent',
+  character_guild_name: 'guild',
+  character_image: 'image_url',
+  character_date_create: 'created_at',
+  access_flag: 'recent_login',
+  liberation_quest_clear_flag: 'is_genesis_owner',
+} as const;
+
+type BasicApiResponseMap = typeof BasicApiResponseMap;
+type InputKeys = keyof BasicApiResponseMap;
+
+type RenameMapped<T> = {
+  [K in keyof T as K extends InputKeys ? BasicApiResponseMap[K] : K]: T[K];
+};
+
+export const renameBasicApiResponse = <T extends Record<string, unknown>>(obj: T): RenameMapped<T> => {
+  const entries = Object.entries(obj) as [keyof T, T[keyof T]][];
+  const mapped = entries.map(([key, value]) => {
+    const mappedKey = (
+      key in BasicApiResponseMap ? BasicApiResponseMap[key as InputKeys] : key
+    ) as keyof RenameMapped<T>;
+
+    return [mappedKey, value] as const;
+  });
+
+  return Object.fromEntries(mapped) as RenameMapped<T>;
+};
---
+++ packages/mapleInsight-types/src/shared/index.ts
@@ -0,0 +1,1 @@
+export type KST = string; // ISO 8601 +09:00 형식의 KST 문자열
