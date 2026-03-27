02069dfc86271f574db5388cbc41c1f7313857be
jo4086 <ghzktm5892@gmail.com>
2026-03-27 23:18:38 +0900

    feat(main): 전역 텐스택쿼리 프로바이더 추가

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      {/* <StrictMode> */}
       <App />
-    </StrictMode>
-  </BrowserRouter>,
+      {/* </StrictMode> */}
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
